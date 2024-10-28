be57461c3252d9f20da2aa9857116c6d4724863b
sailesh-one <sailesh.one@hotmail.com>
2024-10-28 17:51:31 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-	 console.log('okokoookokokook.')
-});
 app.listen(3000, () => console.log('Server ready on port 3000.'));
-
 module.exports = app;
